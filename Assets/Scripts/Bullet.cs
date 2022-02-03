d7cc3a3d484b998899cf6af23d67c50dba37a674
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-02-03 01:50:41 -0300

    [UPDATED] Player shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speedBullet = 20;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.forward * speedBullet * Time.deltaTime);
+        Destroy(gameObject, 5f);
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+    [SerializeField]
+    GameObject bulletSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        }
+    }
+}

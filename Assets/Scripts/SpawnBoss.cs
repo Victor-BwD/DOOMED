24337d3c5dbc01860536f9bdbcbaf749461caa19
VictorBwD <victor.bogdanowdornelles@gmail.com>
2022-05-17 15:50:03 -0300

    [UPDATED] Update mechanic spawn boss now in random places

--- Assets/Scripts/SpawnBoss.cs
+++ Assets/Scripts/SpawnBoss.cs
@@ -8,12 +8,15 @@ public class SpawnBoss : MonoBehaviour
     public float timeBetweenGenerations = 60;
     public GameObject bossPrefab;
     private UIController scriptUIController;
+    public Transform[] possiblePositionsToSpawn;
+    private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         timeToNextGeneration = timeBetweenGenerations;
         scriptUIController = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -21,9 +24,28 @@ public class SpawnBoss : MonoBehaviour
     {
         if(Time.timeSinceLevelLoad > timeToNextGeneration)
         {
-            Instantiate(bossPrefab, transform.position, Quaternion.identity);
+            Vector3 SpawnPosition = CalculeMoreFarDistanceFromThePlayer();
+            Instantiate(bossPrefab, SpawnPosition, Quaternion.identity); // spawn boss in one of spawn location
             scriptUIController.AppearTextBossCreated();
             timeToNextGeneration = Time.timeSinceLevelLoad + timeBetweenGenerations;
         }
+    }
+
+    // Function to calculate the greatest distance between the player and the spawner
+    Vector3 CalculeMoreFarDistanceFromThePlayer()
+    {
+        Vector3 GreatestDistancePosition = Vector3.zero;
+        float GreatestDistanceFounded = 0;
+        foreach(Transform p in possiblePositionsToSpawn)
+        {
+            float distanceBetweenPlayer = Vector3.Distance(p.position, player.position);
+            if(distanceBetweenPlayer > GreatestDistanceFounded)
+            {
+                GreatestDistanceFounded = distanceBetweenPlayer;
+                GreatestDistancePosition = p.position;
+            }
+        }
+
+        return GreatestDistancePosition;
     }
 }
